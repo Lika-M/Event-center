9280be27fc4cdf31acb8a17cc722c0bc511e8e19
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-01-10 18:57:26 +0200

    Fixing of logout implementation.

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,18 +1,18 @@
-import { useContext} from 'react';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import {logout} from '../../services/authService.js'
+import { logout } from '../../services/authService.js'
 import { AuthContext } from '../../contexts/AuthContext.js';
 
 export const Logout = () => {
-    const {deleteUserInfo } = useContext(AuthContext);
+    const { deleteUserInfo } = useContext(AuthContext);
     const navigate = useNavigate();
 
-        logout()
-            .then(() => {
-                deleteUserInfo();
-                navigate('/', { replace: true })
-            });
+    logout()
+        .then(() => {
+            deleteUserInfo();
+            navigate('/', { replace: true })
+        });
 
 
     return null;
