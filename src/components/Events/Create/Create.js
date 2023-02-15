00792b9297e5cadc2a35fc800be7bfa30857742c
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-02-15 11:44:24 +0200

    Changing the access and the name of the form.

--- src/components/Events/Create/Create.js
+++ src/components/Events/Create/Create.js
@@ -2,7 +2,7 @@ import { EventForm } from '../EventForm/EventForm.js';
 
 export const Create = () => {
 
-    const title = 'Add Event';
+    const title = 'Event reservation';
     const btnName = 'ADD EVENT';
 
     return <EventForm title={title} btnName={btnName}/>
