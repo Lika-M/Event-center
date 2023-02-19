fc5dff4a0c36d3f3d79b3cf83d1d155a186695e1
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-02-19 20:38:08 +0200

    Fixing some bugs and making view responsive.

--- src/components/Events/EventItem/EventItem.js
+++ src/components/Events/EventItem/EventItem.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import './EventItem.css'
 
 export const EventItem = ({
-   objectId, topic, date, location
+    objectId, topic, date, location
 }) => {
     const day = date.toString().slice(8, 11);
     const weekDay = new Date(date).toLocaleString('en-us', { weekday: 'long' })
@@ -10,37 +10,36 @@ export const EventItem = ({
     const expired = new Date(date) < new Date();
 
     return (
-      
-            <article className="event-card">
-                <div className="event-card-content">
-                    <div>
-                        <div className="event-card-content-date">
-                            <span>{day}</span>{month.slice(0, 3)}
-                        </div>
-                        <h4 className="event-card-content-title">
-                            <Link to="/calendar/details">
-                                {topic}
-                            </Link>
-                            <span className="event-color"></span>
-                            {expired
-                                ? <span className="event-expired">
-                                    expired
-                                </span>
-                                : ''
-                            }
-                        </h4>
-                    </div>
 
+        <article className="event-card">
+            <div className="event-card-content">
+                <div>
+                    <div className="event-card-content-date">
+                        <span>{day}</span>{month.slice(0, 3)}
+                    </div>
+                    <h4 className="event-card-content-title">
+                        {/* <Link to="/calendar/details"> */}
+                        {topic}
+                        {/* </Link> */}
+                        {expired
+                            ? <span className="event-expired">
+                                expired
+                            </span>
+                            : ''
+                        }
+                    </h4>
                 </div>
-                <div className="event-card-btn-wrapper">
-                    <Link to={`/calendar/event/${objectId}`} className="event-card-link">
-                        EVENT DETAIL
-                    </Link>
-                </div>
-                <div className="event-card-content-detail">
-                    {weekDay}, <span>{location}</span>
-                </div>
-            </article>
-      
+
+            </div>
+            <div className="event-card-btn-wrapper">
+                <Link to={`/calendar/event/${objectId}`} className="event-card-link">
+                    EVENT DETAIL
+                </Link>
+            </div>
+            <div className="event-card-content-detail">
+                <span> {weekDay}, </span><span>{location}</span>
+            </div>
+        </article>
+
     );
 }
