b46d943ced14d68908881e67989f96379cc452f6
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-03-09 12:28:41 +0200

    Changing schemas.

--- src/components/About/Spaces/halls/halls.js
+++ src/components/About/Spaces/halls/halls.js
@@ -4,7 +4,7 @@ export const halls = [
         title: 'Conference Hall',
         subtitle: 'Full set of high-tech presentation equipment, a grandstand, a podium, and a separate space for a booth for simultaneous translation.',
         description: 'Possibility to divide into two separate spaces.',
-        schemas: ['/images/schema-conference.jpg', '/images/schema-forum.jpg'],
+        schemas: ['/images/schema-conference.jpg'],
         capacity: '70',
         style: {backgroundColor: '#5979a9' }
     },
@@ -13,7 +13,7 @@ export const halls = [
         title: 'Library Hall',
         subtitle: 'High-tech presentation equipment.',
         description: 'Suitable for meetings, boards, business meetings, etc.',
-        schemas: ['/images/schema-conference.jpg'],
+        schemas: ['/images/schema-seminar.jpg'],
         capacity: '25',
     },
     {
@@ -21,7 +21,7 @@ export const halls = [
         title: 'Four seasons hall',
         subtitle: 'Suitable for exhibitions and presentations',
         description: '',
-        schemas: ['/images/schema-forum.jpg'],
+        schemas: ['/images/schema-round-table.jpg'],
         capacity: '20',
     },
     {
@@ -29,7 +29,7 @@ export const halls = [
         title: 'Open Space Zone',
         subtitle: 'Suitable for B2B meetings, cocktails, specialized exhibitions, presentations and more.',
         description: '',
-        schemas: ['/images/schema-forum.jpg'],
+        schemas: ['/images/schema-forum.jpg', '/images/schema-round-table.jpg'],
         capacity: '50',
     }
 ]
