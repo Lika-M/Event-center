e92495063739c8c0b629f872725eaaf1020a4910
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-03-15 14:05:41 +0200

    Adding OfferCard component.

--- src/components/About/Offer/OfferCard/OfferCard.js
+++ src/components/About/Offer/OfferCard/OfferCard.js
@@ -1,25 +1,34 @@
 import './OfferCard.css';
 
 export const OfferCard = ({ content, onClose }) => {
-    console.log(content.images)
+
     const onCloseHandler = () => {
         onClose(false);
     }
 
     return (
-        <article className="offer-card">
-            <h2 className="offer-card-title">{content.title}</h2>
-            <p className="offer-card-description">{content.subtitle}</p>
+        <div className="offer-card">
             <div>
-                <h3 className="offer-card-subtitle">{content.description}</h3>
-                <div className="offer-card-images">
-                    {content?.images
-                        .map(x => (<div className="offer-card-img" key={x}>
-                            <img src={x} alt="" />
-                        </div>))}
-                </div>
-                <button onClick={onCloseHandler}>Close</button>
+                <h2 className="offer-card-title">{content.title}</h2>
+                {content.subtitle && <p className="offer-card-description">{content.subtitle}</p>}
             </div>
-        </article >
+
+            {content.text.map(x => (
+                <article>
+                    {x.title && <h3 className="offer-card-subtitle">{x.title}</h3>}
+                    {x.description && <p className="offer-card-description">{x.description}</p>}
+                    <div div className="offer-card-images" >
+                        {
+                            x.images && x.images
+                                .map(x => (<span className="offer-card-img" key={x} >
+                                    <img src={x} alt={x} />
+                                </span>))
+                        }
+                    </div>
+                </article >
+            ))}
+
+            <button onClick={onCloseHandler}>Close</button>
+        </div >
     );
 }
