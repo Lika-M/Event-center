dcb65822732652d18f4d54818894536065a84e1f
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-03-15 14:03:20 +0200

    Adding additional data.

---
+++ src/components/About/Offer/offers.js
@@ -0,0 +1,36 @@
+export const equipment = {
+    title: 'Technical equipment',
+    subtitle: 'Our team will provide you with full technical support according to the concept of each event. All activities related to sound, lighting, projection, translation or technical specification will be realized thanks to the team of professionals of The Innovative Event Center.',
+
+    text: [
+        {
+            description: '',
+            title: 'The latest high tech solutions in the field of communication equipment for maximum security of your event',
+            images: ["/images/security-1.jpg", "/images/security-2.jpg", "/images/security-4.jpg", "/images/security-3.jpg"]
+        },
+        {
+            description: '',
+            title: 'Livestreaming options, webinars and real time tracking of every event in all halls and lobbies of the center',
+            images: ["/images/stream-1.jpg", "/images/stream-2.jpg"]
+        }
+    ]
+
+};
+
+export const service = {
+    title: 'Additional services',
+    subtitle: '',
+    text: [
+        {
+            title: 'Branding and decoration',
+            description: 'Our team works with professionals in the field of branding and decoration, who could give different proposals and options according to the idea and concept of each event.',
+            images: ["/images/branding-1.jpg", "/images/branding-2.jpg", "/images/branding-3.jpg", "/images/branding-4.jpg"]
+        },
+        {
+            title: 'Catering',
+            description: 'Catering services are a key component of any event and should always be presented at a high level. Our team will assist you by offering you an individual catering menu according to your taste and budget. Whether you need a coffee break, a business breakfast, a working lunch or a formal cocktail, our professional team will meet all your expectations.',
+            images: ["/images/catering-1.jpg", "/images/catering-2.jpg", "/images/catering-3.jpg", "/images/catering-4.jpg"]
+        }
+    ]
+
+};
